6c1606c5d8892aeaba086203a71b83b1e6321b01
Laura Darlak <laura@polyporedesign.com>
2018-08-03 17:15:19 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,5 +1,7 @@
 # code your #position_taken? method here!
 
 def position_taken? (board, index)
-  board[index] != " " && board[index] !=  "" && board[index] != nil
+  (board[index] ==  " " || board[index] == "" || board[index] == nil) ?
+    false : true
+  # board[index] != " " && board[index] !=  "" && board[index] != nil
 end
